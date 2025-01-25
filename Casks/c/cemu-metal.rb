a33460d4d88c95f3edd3a5dc8fef6fc3c22ffdc1
Red6785 <rayan.shkour@gmail.com>
2025-01-24 22:59:59 -0600

    cemu-metal 2.2.1

--- Casks/c/cemu-metal.rb
+++ Casks/c/cemu-metal.rb
@@ -1,7 +1,7 @@
 cask "cemu-metal" do
   # Every single version in this fork is a pre-release, so ignore that error.
-  version "2.2.0"
-  sha256 "2dae301877d54fb896700a668c91f6da5ca35cca0c8968f4ae38ac7fcd2a5f23"
+  version "2.2.1"
+  sha256 "7e9dea344e89c1a51ded50c7d0e74d22cb369191681f5ba695570e416a611923"
 
   # This is a fork of the original project that adds in Metal as a graphics backend.
   url "https://github.com/SamoZ256/Cemu/releases/download/testing#{version}/Cemu_release.zip"
