aa35990ae684145c2805855bceac28782eeb8c2f
Red6785 <rayan.shkour@gmail.com>
2025-04-04 15:10:40 -0500

    update ryujinx cask for new urls

--- Casks/r/ryujinx.rb
+++ Casks/r/ryujinx.rb
@@ -2,13 +2,14 @@ cask "ryujinx" do
   version "1.2.82"
   sha256 "20eb23fa8fcd03ea789346d8eeff4b0ccd5e60b019c68ae7069ee318b2517851"
 
-  url "https://github.com/Ryubing/Ryujinx/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
+  url "https://github.com/Ryubing/Stable-Releases/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz",
+      verified: "github.com/Ryubing/Stable-Releases/"
   name "Ryujinx"
   desc "Nintendo Switch emulator written in C#, originally created by gdkchan"
-  homepage "https://github.com/Ryubing/Ryujinx"
+  homepage "https://ryujinx.app/"
 
   livecheck do
-    url :homepage
+    url :url
     strategy :github_latest
   end
 
