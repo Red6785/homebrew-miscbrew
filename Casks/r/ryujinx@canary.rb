c55aaf879e9987a2d3644cc380002830229aa436
Red6785 <rayan.shkour@gmail.com>
2025-04-15 11:06:30 -0500

    add canary ryujinx

---
+++ Casks/r/ryujinx@canary.rb
@@ -0,0 +1,25 @@
+cask "ryujinx@canary" do
+  version "1.3.31"
+  sha256 "09b1848eed94bedaf06455b72d6f9d9e27c42f40e8e72a8045ae4ad5c1f3689c"
+
+  url "https://github.com/Ryubing/Canary-Releases/releases/download/#{version}/ryujinx-canary-#{version}-macos_universal.app.tar.gz",
+      verified: "github.com/Ryubing/Canary-Releases/"
+  name "Ryujinx"
+  desc "EA of a Nintendo Switch emulator written in C#, originally created by gdkchan"
+  homepage "https://ryujinx.app/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "Ryujinx.app"
+
+  zap trash: [
+    "/Library/Application Support/Ryujinx",
+    "~/Library/Preferences/org.ryujinx.Ryujinx.plist",
+    "~/Library/Saved Application State/org.ryujinx.Ryujinx.savedState",
+  ]
+end
