518864d44ddcb1aabe116870512e42ef42162297
Red6785 <rayan.shkour@gmail.com>
2025-05-01 10:04:13 -0500

    harmonoid 0.3.10

--- Casks/h/harmonoid.rb
+++ Casks/h/harmonoid.rb
@@ -1,6 +1,6 @@
 cask "harmonoid" do
-  version "0.3.9"
-  sha256 "f516836e51c185959beb431f4cb4d85a50e6e965342d23a4ed3f54fb73c89058"
+  version "0.3.10"
+  sha256 "eea7149d805ab14aa2b045b9e9fab82468288c199fab272b0c832908258bc657"
 
   url "https://github.com/alexmercerind2/harmonoid-releases/releases/download/v#{version}/harmonoid-macos-universal.dmg",
       verified: "github.com/alexmercerind2/harmonoid-releases/"
