47e571161cb286541e805f805e432db658a17667
Red6785 <rayan.shkour@gmail.com>
2025-05-06 14:31:24 -0500

    add azahar cask

---
+++ Casks/a/azahar.rb
@@ -0,0 +1,26 @@
+cask "azahar" do
+  version "2121"
+  sha256 "7feb4ba1a4e8014752220bb925510ce3619a563d4b15a3f723f86a71ee05a01c"
+
+  url "https://github.com/azahar-emu/azahar/releases/download/#{version}/azahar-#{version}-macos-universal.zip",
+      verified: "github.com/azahar-emu/azahar/"
+  name "azahar"
+  desc "Nintendo 3DS emulator"
+  homepage "https://azahar-emu.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest do |json|
+      json["tag_name"][/(\d+)/, 1]
+    end
+  end
+
+  depends_on macos: ">= :ventura"
+
+  app "azahar-#{version}-macos-universal/Azahar.app"
+
+  zap trash: [
+    "~/Library/Application Support/Azahar",
+    "~/Library/Preferences/org.azahar-emu.azahar.plist",
+  ]
+end
