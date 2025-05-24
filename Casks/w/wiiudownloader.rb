9fc1c859cc3c1877e824180ab64522f1c0fe093d
Red6785 <rayan.shkour@gmail.com>
2025-05-24 13:54:15 -0500

    wiiudownloader 2.64

--- Casks/w/wiiudownloader.rb
+++ Casks/w/wiiudownloader.rb
@@ -1,6 +1,6 @@
 cask "wiiudownloader" do
-  version "2.62"
-  sha256 "ee667a6cec1bd7997b9e2a4695527ae40fd307ae1950154a2907f7d5a50daac6"
+  version "2.64"
+  sha256 "a50223babb57860e9e24f10341d5518c81b319d7b746cedd7ea782ce25ec97a5"
 
   url "https://github.com/Xpl0itU/WiiUDownloader/releases/download/v#{version}/WiiUDownloader-macOS-Universal.dmg"
   name "WiiUDownloader"
