ec620f1e63cc607beb0c449ae34eea148aab483a
Red6785 <rayan.shkour@gmail.com>
2025-06-02 13:43:51 -0500

    add KenjiNX

---
+++ Casks/k/kenjinx.rb
@@ -0,0 +1,25 @@
+cask "kenjinx" do
+  version "2.0.4"
+  sha256 "4523d74788c227917d00ede6956b38920ec542df365899d527db09f3b984e851"
+
+  url "https://github.com/Kenji-NX/Releases/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz",
+      verified: "github.com/Kenji-NX/Releases/"
+  name "kenjinx"
+  desc "Nintendo Switch emulator"
+  homepage "https://git.ryujinx.app/kenji-nx/ryujinx"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "Ryujinx.app", target: "KenjiNX.app"
+
+  zap trash: [
+    "/Library/Application Support/Ryujinx",
+    "~/Library/Preferences/org.ryujinx.Ryujinx.plist",
+    "~/Library/Saved Application State/org.ryujinx.Ryujinx.savedState",
+  ]
+end
